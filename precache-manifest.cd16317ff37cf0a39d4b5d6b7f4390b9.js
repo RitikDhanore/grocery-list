e63e1cdd5944a65e121eb8d8a0cb2ff2a1c8dee9
Ritik <ritik002dhanore@gmail.com>
2020-09-25 21:57:12 +0530

    Updates

---
+++ precache-manifest.cd16317ff37cf0a39d4b5d6b7f4390b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92dfbbfb285e0aa1d65904bac4c5e94c",
+    "url": "/grocery-list/index.html"
+  },
+  {
+    "revision": "03905d65d068b1c0e35f",
+    "url": "/grocery-list/static/js/2.089d3b9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/grocery-list/static/js/2.089d3b9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77b8a4c20533ec55bc3f",
+    "url": "/grocery-list/static/js/main.1e07196c.chunk.js"
+  },
+  {
+    "revision": "26dbfb5cc40c270363a4",
+    "url": "/grocery-list/static/js/runtime-main.2a4d0231.js"
+  }
+]);
